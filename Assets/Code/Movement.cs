dc85b8b12dd206fa94d054cf6743013f831c0820
carlawatts <130099394+carlawatts@users.noreply.github.com>
2024-03-01 16:55:38 -0700

    Add files via upload

---
+++ Assets/Code/Movement.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(CharacterController))]
+public class Movement : MonoBehaviour
+{
+  public float playerSpeed = 7;
+  public float rotationSpeed = 220;
+  public float grav = -20f;
+  public float jumpSpeed = 7;
+  public float pushForce = 3.0f;
+  private ControllerColliderHit contact;
+  CharacterController characterController;
+  Vector3 moveVelocity;
+  Vector3 turnVelocity;
+
+  private bool playerGrounded = true;
+  void Awake()
+  {
+    characterController = GetComponent<CharacterController>();
+  }
+  void Update() {
+
+    bool wKeyDown = Input.GetKey(KeyCode.W);
+    bool aKeyDown = Input.GetKey(KeyCode.A);
+    bool dKeyDown = Input.GetKey(KeyCode.D);
+    
+    if(characterController.isGrounded) {
+      int playerFor = 0;
+      int playerRot = 0;
+      
+      if (wKeyDown) {
+        playerFor = 1;
+      } else {
+        playerFor = 0;
+      }
+
+      if (aKeyDown) {
+        playerRot = -1;
+      } else if (dKeyDown) {
+        playerRot = 1;
+      } else {
+        playerRot = 0;
+      }
+      
+      moveVelocity = transform.forward * playerSpeed * playerFor;
+      turnVelocity = transform.up * rotationSpeed * playerRot;
+      playerGrounded = true;
+    }
+    if(Input.GetKey(KeyCode.C) && playerGrounded)
+    {
+      moveVelocity.y = jumpSpeed;
+      playerGrounded = false;
+    }
+    //Adding gravity
+    moveVelocity.y += grav * Time.deltaTime;
+    characterController.Move(moveVelocity * Time.deltaTime);
+    transform.Rotate(turnVelocity * Time.deltaTime);
+  }
+
+  void OnControllerColliderHit(ControllerColliderHit hit){
+    contact = hit;
+    Rigidbody body = hit.collider.attachedRigidbody;
+    if (body != null && !body.isKinematic){
+      body.velocity = hit.moveDirection * pushForce;
+    }
+  }
+}
---
+++ Assets/Code/Movement2.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(CharacterController))]
+public class Movement2 : MonoBehaviour
+{
+  public float playerSpeed = 7;
+  public float rotationSpeed = 220;
+  public float grav = -20f;
+  public float jumpSpeed = 7;
+    public float pushForce = 3.0f;
+  private ControllerColliderHit contact;
+  CharacterController characterController;
+  Vector3 moveVelocity;
+  Vector3 turnVelocity;
+
+  private bool playerGrounded = true;
+  void Awake()
+  {
+    characterController = GetComponent<CharacterController>();
+  }
+  void Update() {
+
+    bool iKeyDown = Input.GetKey(KeyCode.I);
+    bool jKeyDown = Input.GetKey(KeyCode.J);
+    bool lKeyDown = Input.GetKey(KeyCode.L);
+    
+    if(characterController.isGrounded) {
+      int playerFor = 0;
+      int playerRot = 0;
+      
+      if (iKeyDown) {
+        playerFor = 1;
+      } else {
+        playerFor = 0;
+      }
+
+      if (jKeyDown) {
+        playerRot = -1;
+      } else if (lKeyDown) {
+        playerRot = 1;
+      } else {
+        playerRot = 0;
+      }
+      
+      moveVelocity = transform.forward * playerSpeed * playerFor;
+      turnVelocity = transform.up * rotationSpeed * playerRot;
+      playerGrounded = true;
+    }
+    if(Input.GetKey(KeyCode.N) && playerGrounded)
+    {
+      moveVelocity.y = jumpSpeed;
+      playerGrounded = false;
+    }
+    //Adding gravity
+    moveVelocity.y += grav * Time.deltaTime;
+    characterController.Move(moveVelocity * Time.deltaTime);
+    transform.Rotate(turnVelocity * Time.deltaTime);
+  }
+
+    void OnControllerColliderHit(ControllerColliderHit hit){
+    contact = hit;
+    Rigidbody body = hit.collider.attachedRigidbody;
+    if (body != null && !body.isKinematic){
+      body.velocity = hit.moveDirection * pushForce;
+    }
+  }
+}
